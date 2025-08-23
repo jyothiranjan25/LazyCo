d1d4b9618c92ea0b69ff02b2b28c2e0e95dabcb2
Jyothiranjan <Jyothiranjan@dsjkeeplearning.com>
2025-08-23 20:58:52 +0530

    removed unnessary

--- src/main/java/com/example/lazyco/backend/core/web/BackendWebConf.java
+++ src/main/java/com/example/lazyco/backend/core/web/BackendWebConf.java
@@ -15,31 +15,9 @@ import java.util.List;
 @Configuration
 @EnableScheduling
 @ComponentScan(basePackages = {"com.example.lazyco.backend"})
-@ImportResource({
-        "classpath:ApplicationContext.xml",
-        "classpath:DatabaseContext.xml",
-})
 @EnableTransactionManagement
 @EnableAsync(proxyTargetClass = true)
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @PropertySources({@PropertySource("classpath:application.properties")})
-public class BackendWebConf {
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        // This bean is necessary to resolve @Value and ${...}
-        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
-
-        // Add the properties file to the list of resources
-        List<ClassPathResource> resources = new ArrayList<>();
-        resources.add(new ClassPathResource("application.properties"));
-
-        // Set the location of the properties file
-        configurer.setLocations(resources.toArray(new ClassPathResource[0]));
-        configurer.setFileEncoding("UTF-8"); // Set the file encoding
-        configurer.setIgnoreUnresolvablePlaceholders(false); // Ignore unresolvable placeholders
-        configurer.setIgnoreResourceNotFound(false); // Ignore resource not found
-        return configurer;
-    }
-}
+public class BackendWebConf {}
 
